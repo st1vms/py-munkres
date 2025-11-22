c2e7ece03c49c0b88bf35ea40df099e8c1972329
st1vms <96783398+st1vms@users.noreply.github.com>
2025-11-22 13:28:46 +0100

    Added unittest

--- tests/test_optimal.py
+++ tests/test_optimal.py
@@ -1,7 +1,5 @@
 from pymunkres.pymunkres import munkres
-
-A = [1, 3, 6, 8]
-B = [2, 4, 5, 7]
+import unittest
 
 
 def _difference(x: int | float, y: int | float) -> int | float:
@@ -12,18 +10,50 @@ def _linear_func_one(x: int | float, y: int | float) -> int | float:
     return abs(x - y) + (x + y) % 3
 
 
-def test_optimal():
+class Test_Optimal_MXSquare(unittest.TestCase):
 
-    res = munkres(A, B, _difference)
-    assert res[-1]
+    A = [1, 3, 6, 8]
+    B = [2, 4, 5, 7]
 
-    print(f"{A=}, {B}, {res=}")
+    def test_optimal(self):
 
-    res = munkres(A, B, _linear_func_one)
-    assert res[-1]
+        res = munkres(self.A, self.B, _difference)
+        print(f"A={self.A}, B={self.B} -> M={res[0]}, I={res[1]}")
+        self.assertTrue(res[-1])
 
-    print(f"{A=}, {B}, {res=}")
+        res = munkres(self.A, self.B, _linear_func_one)
+        print(f"A={self.A}, B={self.B} -> M={res[0]}, I={res[1]}")
+        self.assertTrue(res[-1])
+
+
+class Test_Optimal_MXRectangle(unittest.TestCase):
+
+    A1 = [1, 3, 6, 8]
+    B1 = [2, 4]
+
+    A2 = [1, 3]
+    B2 = [2, 4, 5, 7]
+
+    def test_optimal_less_jobs(self):
+
+        res = munkres(self.A1, self.B1, _difference)
+        print(f"A={self.A1}, B={self.B1} -> M={res[0]}, I={res[1]}")
+        self.assertTrue(res[-1])
+
+        res = munkres(self.A1, self.B1, _linear_func_one)
+        print(f"A={self.A1}, B={self.B1} -> M={res[0]}, I={res[1]}")
+        self.assertTrue(res[-1])
+
+    def test_optimal_less_workers(self):
+
+        res = munkres(self.A2, self.B2, _difference)
+        print(f"A={self.A2}, B={self.B2} -> M={res[0]}, I={res[1]}")
+        self.assertTrue(res[-1])
+
+        res = munkres(self.A2, self.B2, _linear_func_one)
+        print(f"A={self.A2}, B={self.B2} -> M={res[0]}, I={res[1]}")
+        self.assertTrue(res[-1])
 
 
 if __name__ == "__main__":
-    test_optimal()
+    unittest.main()
